ef898487ce10b65b9290b17f034ec7bc4a7225a3
2466978687@qq.com <2466978687@qq.com>
2021-01-28 15:22:48 +0800

    第一次提交

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/Hello.java
@@ -0,0 +1,8 @@
+package com.atguigu.springcloud;
+
+/**
+ * @auther liwenshan@ygsoft.com
+ * @create 2021-01-25 12:13
+ */
+public class Hello {
+}
---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,6 @@
+package com.atguigu.springcloud;
+
+@SpringBootApplication
+public class PaymentMain8001 {
+    
+}
